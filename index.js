78635fe684774bb0ece836e5494a82c0d57aad0c
Mohd Aqib <aqib@logicwind.com>
2020-12-02 21:24:04 +0530

    index file setup done

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan')
+const bodyParser = require('body-parser');
+
+require('./models/Student');
+require('./models/Subject');
+
+mongoose.connect(keys.mongoURI, { useMongoClient: true })
+  .then(() => console.log('Database connected successfully...'))
+  .catch((e) => console.log('err in connecting db: ', e))
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }))
+
+require('./routes/authRoutes')(app);
+require('./routes/blogRoutes')(app);
+
+app.use(morgan('dev', {
+    skip: function (req, res) { return res.statusCode < 400 }
+  }))
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Listening on port`, PORT);
+});
